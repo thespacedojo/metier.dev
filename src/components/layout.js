43bb2eeb4e10df781883d95aedefd0e749b5dcbe
Josh Owens <josh.owens@gmail.com>
2019-05-05 12:57:29 -0700

    Fix the sticky footer design

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,17 +14,32 @@ import Header from "./header"
 import "./layout.css"
 
 const footer = css`
-  grid-row-start: 4;
-  text-align: center;
+  display:flex;
+  flex-shrink: 0;
+  height: 4rem;
+  color: white;
+  background-color: #222;
+  align-items: center;
+  justify-content: center;
+
+  a, a:visited {
+    color: cornflowerblue
+  }
 `
 
-const grid = css`
+const main = css`
   margin: 0 auto;
-  max-width: 960;
   padding: 0px 1.0875rem 1.45rem;
   padding-top: 0;
-  width: 100vw;
+  flex: 1 0 auto;
 `
+
+const flexer = css`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
@@ -37,17 +52,13 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <div>
-        <div
-          css={grid}
-        >
-          <main >{children}</main>
-          <footer css={footer}>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+      <div css={flexer}>
+        <main css={main} >{children}</main>
+        <footer css={footer}>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
       </div>
     )}
   />
